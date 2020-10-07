041f142c6e6cc337922e846f49918512fd9a16f6
stepin105010 <69056143+stepin105010@users.noreply.github.com>
2020-10-07 23:30:17 +0530

    Add files via upload

---
+++ 03_Implementation/main.c
@@ -0,0 +1,38 @@
+#include <library.h>
+struct meroDate
+{
+int mm,dd,yy;
+};
+struct books
+{
+int id;
+char stname[20];
+char name[20];
+char Author[20];
+int quantity;
+float Price;
+int count;
+int rackno;
+char *cat;
+struct meroDate issued;
+struct meroDate duedate;
+};
+struct books a;
+void main()
+{
+k=Password(password);
+if(k==1)
+{
+mainmenu();
+}
+else
+{
+printf("login unsuccessfull");
+
+
+}
+main();
+getch();
+//return 0;
+
+}
